73e6f7a70995277c5cea51b16321b41bff039f0d
Farouq Ayofe <109066931+farouq-ayofe@users.noreply.github.com>
2025-11-27 18:25:40 +0000

    match result

--- src/routes/list_tasks_route.rs
+++ src/routes/list_tasks_route.rs
@@ -1,18 +1,22 @@
+use crate::route_utils::{api_response::ApiResponse, create_json_body::create_json_body, api_error::ApiError};
 use axum::extract::State;
-use crate::route_utils::{api_response::ApiResponse, create_json_body::create_json_body};
 use serde_json::{Value, json};
-use std::sync::Arc; 
 use sqlx::{pool::Pool, postgres::Postgres};
+use std::sync::Arc;
 
 use crate::queries::{list_task_query::LIST_TASKS_QUERY, task::Task};
-
-
 
 pub async fn list_tasks_route(State(pool): State<Arc<Pool<Postgres>>>) -> ApiResponse {
     let tasks = sqlx::query_as::<_, Task>(LIST_TASKS_QUERY)
         .fetch_all(pool.as_ref())
-        .await?;
- 
+        .await;
 
-    Ok(create_json_body(json!(tasks), Value::Null))
+    match tasks {
+        Ok(tasks) => Ok(create_json_body(json!(tasks), Value::Null)),
+        Err(e) => {
+            eprintln!("Error fetching tasks: {:?}", e);
+
+            Err(ApiError::InternalError)
+        },
+    }
 }
